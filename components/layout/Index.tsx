4f6b039022b6d84ceac0e014639fe9e7e4a0bcd4
Justin George <justin.george@surveysparrow.com>
2022-09-12 23:03:56 +0530

    added split bee

--- components/layout/Index.tsx
+++ components/layout/Index.tsx
@@ -1,14 +1,26 @@
+import { useEffect } from "react";
 import Header from "./Header";
+import splitbee from '@splitbee/web';
 
-const Layout = ({ children }) => (
-  <div>
-    <Header />
-    <main>
-      <div className="container max-w-3xl mx-auto my-12 p-4">
-        {children}
-      </div>
-    </main>
-  </div>
-)
+const Layout = ({ children }) => {
+
+  useEffect(() => {
+    splitbee.init({
+      scriptUrl: "https://cdn.splitbee.io/sb.js",
+      apiUrl: "https://hive.splitbee.io",
+    })
+  }, []);
+
+  return (
+    <div>
+      <Header />
+      <main>
+        <div className="container max-w-3xl mx-auto my-12 p-4">
+          {children}
+        </div>
+      </main>
+    </div>
+  )
+}
 
 export default Layout;

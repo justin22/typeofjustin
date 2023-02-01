d905630559b7e3069feda96ffc30dac399834de9
Justin George <justin.george@surveysparrow.com>
2023-02-01 09:05:41 +0530

    moved to content layer

--- pages/404.tsx
+++ pages/404.tsx
@@ -1,9 +1,11 @@
 import Link from "next/link";
 
-const ErrorPage: React.FC  = () => (
+const ErrorPage: React.FC = () => (
   <div className="min-h-screen">
-    <h1 className="text-4xl"> Page not found </h1>
-    <p className="mt-4">
+    <h1 className="text-4xl md:text-6xl tracking-wide font-normal md:font-normal mb-4 text-gray-700 dark:text-gray-200">
+      Page not found
+    </h1>
+    <p className="mt-4 text-gray-600 dark:text-gray-300">
       The page you are looking for does not exist anymore, on never existed.
     </p>
 

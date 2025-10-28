7e01c5733c50454f732d953d70c9840124287b73
Justin <georgejustin22@gmail.com>
2025-10-28 13:18:40 +0530

    Update intro.tsx

--- components/home/intro.tsx
+++ components/home/intro.tsx
@@ -6,7 +6,7 @@ export function Introduction() {
       <p className="text-gray-800 mb-6"> Hello, I&apos;m Justin</p>
 
       <p className="text-gray-800 leading-8">
-        I <Link href="/career"><a className="underline decoration-sky-500/30 cursor-pointer font-medium hover:decoration-pink-500/30 transition-all duration-150">work</a></Link> at SurveySparrow,  passionate about UX and in the process of learning more about it.
+        I <Link href="/career"><a className="underline decoration-sky-500/30 cursor-pointer font-medium hover:decoration-pink-500/30 transition-all duration-150">work</a></Link> at <a className="underline decoration-sky-500/30 cursor-pointer font-medium hover:decoration-pink-500/30 transition-all duration-150" target="_blank">SparrowDesk</a>,  passionate about UX and in the process of learning more about it.
       </p>
     </div>
   )
